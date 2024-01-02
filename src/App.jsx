f74ff5f06dee47818f4998f269b913cbd677025c
Ruben Godoy <105462292+RubenG73@users.noreply.github.com>
2024-01-02 19:51:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import About from './components/About'
 import Interests from './components/Interests'
 import Footer from './components/Footer'
-import Info from './components/info'
+import Info from './components/Info'
 
 function App() {
   return(
